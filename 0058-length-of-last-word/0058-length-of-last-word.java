2d676491746409f2e3151ba21d3971069fea0708
Ayush Singh Solanki <ayushsolanki.pis9@gmail.com>
2026-08-19 18:51:56 +0530

    Time: 0 ms (100%), Space: 43.1 MB (33.29%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+       int count = 0;
+        int i = s.length() - 1;
+
+        // Ending spaces skip karo
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+
+        // Last word count karo
+        while (i >= 0 && s.charAt(i) != ' ') {
+            count++;
+            i--;
+        }
+
+        return count;
+
+        
+    }
+}
