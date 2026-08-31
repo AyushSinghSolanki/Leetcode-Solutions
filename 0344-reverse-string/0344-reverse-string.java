d0fba68c71861176b1be72e868710b49dfd7d5a5
Ayush Singh Solanki <ayushsolanki.pis9@gmail.com>
2026-08-31 23:50:44 +0530

    Time: 1 ms (22.66%), Space: 48.6 MB (7.61%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,16 +1,20 @@
 class Solution {
     public void reverseString(char[] s) {
-        int left = 0;
-        int right = s.length-1;
-        while(left<right){
-            char temp = s[left];
-            s[left] = s[right];
-            s[right] = temp;
-            left++;
-            right--;
+        reverse(s, 0, s.length - 1);
+    }
 
+    private void reverse(char[] s, int left, int right) {
+        // Base case
+        if (left >= right) {
+            return;
         }
 
+        // Swap
+        char temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
 
+        // Recursive call
+        reverse(s, left + 1, right - 1);
     }
 }
