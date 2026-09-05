f0ace269e20b5475480a130b624890a0a08c6f32
Ayush Singh Solanki <ayushsolanki.pis9@gmail.com>
2026-09-05 12:36:54 +0530

    Time: 0 ms (100%), Space: 43.7 MB (33.84%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,29 +1,33 @@
 
 class Solution {
-     static void solve(String digits, int index, String[] mapping, ArrayList<String> result,StringBuilder output ){
-        //base case
-        if(index >= digits.length()){
-            result.add(output.toString());
-            return;
-        }
-        int value = digits.charAt(index) - '0';
-        String mappedString = mapping[value];
 
-        for(int i =0; i<mappedString.length(); i++){
-            output.append(mappedString.charAt(i));
-         solve(digits, index+1, mapping, result, output);
-         output.deleteCharAt(output.length()-1);
-        }
-     }
+   static void solve(String digits, int index, String [] mapping, ArrayList<String> result,StringBuilder output   ){
+    // sbse phela kaam base case
+    if(index >= digits.length()){
+        result.add(output.toString());
+        return ;
+    }
+         
+         int value = digits.charAt(index) - '0';
+         String mappedString = mapping[value];
 
+
+         for(int i = 0; i < mappedString.length(); i++ ){
+             output.append(mappedString.charAt(i));
+              solve(digits, index + 1, mapping, result, output);
+               output.deleteCharAt(output.length() - 1);
+
+         }
+   }
 
     public List<String> letterCombinations(String digits) {
-    String [] mapping = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
-    ArrayList<String> result = new ArrayList<>();
-    int index = 0;
-    StringBuilder output = new  StringBuilder();
+      String [] mapping = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+      ArrayList <String> result = new ArrayList<>();
 
-     solve(digits, index, mapping, result, output);
-     return result;
+      if(digits.length() == 0){
+            return result;
+        }
+            solve(digits, 0,  mapping, result, new StringBuilder());
+            return result;
     }
 }
