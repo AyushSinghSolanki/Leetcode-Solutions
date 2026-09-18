02cc4062be2cf1901b33f7c9e1bdf7b40c400eaf
Ayush Singh Solanki <ayushsolanki.pis9@gmail.com>
2026-09-18 23:27:24 +0530

    Time: 1 ms (87.16%), Space: 43.9 MB (87.75%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,15 +1,18 @@
 class Solution {
 
    static void solve(int[] nums, int index, ArrayList<Integer> output, List<List<Integer>> result ){
-    // Base case
+    // Base case // jha apn ko rukna hota h 
     if(index == nums.length){
             result.add(new ArrayList<>(output));
             return;
     }
+        // ek step apn krenge baaki recursion sbhal lega 
         
         output.add(nums[index]);
         solve(nums, index + 1, output, result);
         output.remove(output.size() - 1);
+
+        //recurive step 
         solve(nums, index + 1, output, result);
    }
 
